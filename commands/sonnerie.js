08588ce1fa54875154a3279e1ebb719e82823b46
Jules <167612505+Jules-chv@users.noreply.github.com>
2025-04-15 22:34:56 +0200

    Update sonnerie.js

--- commands/sonnerie.js
+++ commands/sonnerie.js
@@ -1,6 +1,7 @@
 const { SlashCommandBuilder } = require('discord.js');
 const { guildId, categorieCible, salonsSpecifiques } = require('../config.json');
 const { createAudioPlayer, createAudioResource, joinVoiceChannel, VoiceConnectionStatus } = require('@discordjs/voice');
+const path = require('path');
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -10,31 +11,32 @@ module.exports = {
   async execute(interaction) {
     const guild = interaction.client.guilds.cache.get(guildId);
 
-    const categoryChannels = guild.channels.cache.filter(channel =>
+    const voiceChannels = guild.channels.cache.filter(channel =>
       (categorieCible.includes(channel.parentId) || salonsSpecifiques.includes(channel.id)) &&
-      channel.type === 2 // Type 2 = GUILD_VOICE
+      channel.type === 2 // GUILD_VOICE
     );
 
-    for (const channel of categoryChannels.values()) {
+    for (const channel of voiceChannels.values()) {
       if (channel.members.size === 0) continue;
 
       const connection = joinVoiceChannel({
         channelId: channel.id,
         guildId: guild.id,
-        adapterCreator: guild.voiceAdapterCreator
+        adapterCreator: guild.voiceAdapterCreator,
       });
 
       connection.on(VoiceConnectionStatus.Ready, () => {
         const player = createAudioPlayer();
-        const path = require('path');
-        const audioPath = type
-          ? path.join(__dirname, `../audios/alarme_${type}.mp3`)
-          : path.join(__dirname, '../audios/sonnerie.mp3');
-
-const resource = createAudioResource(audioPath);
+        const audioPath = path.join(__dirname, '../audios/sonnerie.mp3');
+        const resource = createAudioResource(audioPath);
 
         player.play(resource);
         connection.subscribe(player);
+
+        // Déconnecte après la fin de l’audio
+        player.on('idle', () => {
+          connection.destroy();
+        });
       });
     }
 
